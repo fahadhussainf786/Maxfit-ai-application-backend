5d49d4f385c3582fcc03f8b6c705bd6ea4725ceb
Fahad Hussain <fahadhussainf786@yahoo.com>
2026-03-05 12:23:29 +0500

    changes

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from motor import motor_asyncio  # MongoDB async driver
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 import secrets 
 import hashlib # For hashing passwords
 import os
